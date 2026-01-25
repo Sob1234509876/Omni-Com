d795ab774126bbf058e9fa110791d11370e1daf2
Jacob Zhang Jingwen <NewGuy3141@163.com>
2026-01-25 14:00:38 +0800

    Created OC

---
+++ projects/oc/oc-engine/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module osl.projects.oc.engine {
+    requires static lombok;
+    requires java.desktop;
+    exports io.github.sob1234509876.oc.engine;
+    exports io.github.sob1234509876.oc.engine.internal;
+    exports io.github.sob1234509876.oc.engine.type;
+}
